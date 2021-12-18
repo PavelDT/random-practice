f44e764dec9b175e0886a524dd7b00f9fc1e22b5
PavelDT <paveldt2@gmail.com>
2021-12-18 15:46:27 +0000

    updated varargs output message

--- src/com/github/paveldt/CalcApp.java
+++ src/com/github/paveldt/CalcApp.java
@@ -7,6 +7,6 @@ public class CalcApp {
      */
     public static void main(String[] args) {
         Calc obj = new Calc();
-        System.out.println(obj.add(3, 4, 5, 8));
+        System.out.println(obj.add(3, 4, 5, 8) + " is the sum of all the values");
     }
 }
