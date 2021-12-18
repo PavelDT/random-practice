370b72a4a0889d7248426787e5192f32ebd624c3
PavelDT <paveldt2@gmail.com>
2021-12-18 15:40:35 +0000

    added javadoc comment to main

--- src/com/github/paveldt/CalcApp.java
+++ src/com/github/paveldt/CalcApp.java
@@ -1,6 +1,10 @@
 package com.github.paveldt;
 
 public class CalcApp {
+    /**
+     * Main entry point to the application
+     * @param args
+     */
     public static void main(String[] args) {
         Calc obj = new Calc();
         System.out.println(obj.add(3, 4, 5, 8));
